884350b765c3c4f80f734c32ee300f1451bd66e7
Damien Baty <damien@damienbaty.com>
2014-06-17 13:31:03 +0200

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,4 @@
+# This Makefile requires an internal tool. Sorry.
+PACKAGE = dokang_pdf
+
+include $(shell makefile_path python.mk)
---
+++ tests.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+from unittest import TestCase
+
+from dokang_pdf import PdfHarvester
+
+
+print os.path.dirname(__file__)
+
+
+class TestPdfHarvester(TestCase):
+
+    def test_basics(self):
+        path = os.path.join(os.path.dirname('.'), 'test.pdf')
+        harvester = PdfHarvester()
+        doc = harvester.harvest_file(path)
+        self.assertEqual(doc['title'], "The title")
+        self.assertEqual(doc['content'], "This is the content with a diacritic: à.")
